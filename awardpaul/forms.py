418e32805912125653628ed1db43375bbd6792cb
Paul Munyao <paulmunyao094@gmail.com>
2022-04-09 13:43:51 +0300

    Add forms

--- awardpaul/forms.py
+++ awardpaul/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from django.contrib.auth.models import User
 
-class login_form(forms.Form):
+
+class User(forms.ModelForm):
     username = forms.CharField(label='Username', max_length=100)
     password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
 
     class Meta:
+        model = User
         fields = ('username', 'password')
      
--- awardpaul/views.py
+++ awardpaul/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from  awardpaul.forms import User
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def login(request):
-    return render(request, 'login.html')
+    form = User()
+    return render(request, 'registration/login.html', {'form': form})
+
+    
 
 def register(request):
-    return render(request, 'register.html')        
+    return render(request, 'registration/register.html')        
