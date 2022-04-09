edff9e551250a79cf49e91ecad8a42b7291fab71
Paul Munyao <paulmunyao094@gmail.com>
2022-04-09 22:50:26 +0300

    Import project models

--- awardpaul/forms.py
+++ awardpaul/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Project
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,5 +10,5 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
   
-    
