70955aa49cde6a0d79d9215476e9191c90f4d423
Paul Munyao <paulmunyao094@gmail.com>
2022-04-09 14:26:43 +0300

    Create logout path

--- awardpaul/forms.py
+++ awardpaul/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegisterForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
--- awardpaul/views.py
+++ awardpaul/views.py
@@ -1,20 +1,40 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
-from  awardpaul.forms import User
+from .forms import RegisterForm
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('index')
+            else:
+                return render(request, 'registration/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'registration/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'registration/login.html')
 
+   
 
 def register(request):
     if request.method == 'POST':
-        form = User(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
     else:
-        form = User()
+        form = RegisterForm()
     return render(request, 'registration/register.html', {'form': form})
-           
+
+def logout(request):
+    return redirect('index') 
