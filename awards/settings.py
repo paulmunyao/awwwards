35962661bdbc6025d3bdd5790ede2b2c438093ce
Paul Munyao <paulmunyao094@gmail.com>
2022-04-09 21:47:11 +0300

    Configuring main app to the project to access templates

--- awards/settings.py
+++ awards/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'awards.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'awardpaul/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
