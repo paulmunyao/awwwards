6153df1a6e80bfb46cf3c10958e525dcffb91139
Paul Munyao <paulmunyao094@gmail.com>
2022-04-09 14:10:49 +0300

    Creating a class ofr users to signup

--- awardpaul/forms.py
+++ awardpaul/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
-class User(forms.ModelForm):
-    username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ['username', 'email', 'password1', 'password2']
      
--- awardpaul/views.py
+++ awardpaul/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
 from  awardpaul.forms import User
 
@@ -6,14 +6,6 @@ from  awardpaul.forms import User
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
-    if request.method == 'POST':
-        form = User(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = User()
-    return render(request, 'registration/login.html', {'form': form})
 
 
 def register(request):
@@ -21,6 +13,7 @@ def register(request):
         form = User(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
     else:
         form = User()
     return render(request, 'registration/register.html', {'form': form})
