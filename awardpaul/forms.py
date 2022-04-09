64690240461370cbd230627ceaa946599e7a0958
Paul Munyao <paulmunyao094@gmail.com>
2022-04-09 08:35:13 +0300

    Add class meta where configurations are done for input login fields

--- awardpaul/forms.py
+++ awardpaul/forms.py
@@ -3,4 +3,7 @@ from django import forms
 class login_form(forms.Form):
     username = forms.CharField(label='Username', max_length=100)
     password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
+
+    class Meta:
+        fields = ('username', 'password')
      
