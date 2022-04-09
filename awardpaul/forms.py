1bdad2c75081424c2deb3b14be220a2e40e64648
Paul Munyao <paulmunyao094@gmail.com>
2022-04-09 08:34:01 +0300

    create input files for django for login

--- awardpaul/forms.py
+++ awardpaul/forms.py
@@ -1,1 +1,6 @@
 from django import forms
+
+class login_form(forms.Form):
+    username = forms.CharField(label='Username', max_length=100)
+    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
+     
