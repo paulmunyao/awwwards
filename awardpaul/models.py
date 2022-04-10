6f6bd2b329cee7443b502f443bbbc5f83e09d3bc
Paul Munyao <paulmunyao094@gmail.com>
2022-04-10 15:17:10 +0300

    Add a url link for the project

--- awardpaul/models.py
+++ awardpaul/models.py
@@ -7,5 +7,5 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     title = models.CharField(max_length=100)
     image = CloudinaryField('image')
-    description = models.TextField()
-    link = models.URLField()
+    description = models.TextField(max_length=100)
+    link = models.URLField(max_length=100)
