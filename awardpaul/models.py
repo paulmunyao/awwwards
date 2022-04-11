22b9fca967000339ebcdf6100e939940c411ae03
Paul Munyao <paulmunyao094@gmail.com>
2022-04-11 21:08:47 +0300

    Update models by adding rating choices

--- awardpaul/models.py
+++ awardpaul/models.py
@@ -17,6 +17,19 @@ class Profile(models.Model):
     bio = models.TextField(max_length=100)
     contact = models.TextField(max_length=100)
 
+RATE_CHOICES = [
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+    ('7', '7'),
+    ('8', '8'),
+    ('9', '9'),
+    ('10', '10'),
+]
+
 class Rate(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE )
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
