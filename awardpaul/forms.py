8b6f3b83cbe1b63b9ed074f350af330d01335252
Paul Munyao <paulmunyao094@gmail.com>
2022-04-11 21:19:43 +0300

    Create a form where a user can cdo ratings

--- awardpaul/forms.py
+++ awardpaul/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Project
+from .models import Project, Profile, Rate, RATE_CHOICES
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -16,3 +16,8 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'image', 'description', 'link']  
+
+class RateForm(ModelForm):
+    class Meta:
+        model = Rate
+        fields = ['design', 'usability', 'content']        
